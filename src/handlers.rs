9dce6060745a9421c9fa618337686b051b99b654
Rrogntudju <mario.prince@outlook.com>
2020-01-06 22:32:29 -0500

    actix-web 2

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,105 +1,97 @@
-use actix_web::{web::Payload, HttpResponse, error::{Error, PayloadError}};
-use futures::{Future, future, Stream};
+use actix_web::{error::Error, web::BytesMut, web::Payload, HttpResponse};
+use futures::StreamExt;
 use serde::{Deserialize, Serialize};
-use bytes::Bytes;
 
 mod sudoku;
-use sudoku::{Sudoku, PuzzleError};
+use sudoku::{PuzzleError, Sudoku};
 
 #[derive(Deserialize)]
 struct SudokuRequest {
-    puzzle: String
+    puzzle: String,
 }
 
 #[derive(Serialize)]
 struct SolveResponse {
     status: String,
     data: String,
-    message: String
-}    
+    message: String,
+}
 
 #[derive(Serialize)]
 struct DisplayResponse {
     status: String,
     data: Vec<String>,
-    message: String
-}    
-
-fn solve_sudoku(payload: Result<Bytes, PayloadError>) -> impl Future<Item=String, Error=PuzzleError> {
-    match payload {
-        Ok(body) => {
-            let body_content = std::str::from_utf8(&body).unwrap();
-            let sudoku_puzzle: Result<SudokuRequest, _> = serde_json::from_str(body_content);
-            match sudoku_puzzle {
-                Ok(sp) => {
-                    let solution = Sudoku::new().solve(&sp.puzzle);
-                    match solution {
-                        Ok(s)   => future::ok(s),
-                        Err(e)  => future::err(e)
-                    }
-                }
-                _ => future::err(PuzzleError::InvalidGrid)
-            } 
-        } 
-        _ => future::err(PuzzleError::InvalidGrid)
-    } 
+    message: String,
 }
 
-fn display_sudoku(payload: Result<Bytes, PayloadError>) -> impl Future<Item=Vec<String>, Error=PuzzleError> {
-    match payload {
-        Ok(body) => {
-            let body_content = std::str::from_utf8(&body).unwrap();
-            let sudoku_puzzle: Result<SudokuRequest, _> = serde_json::from_str(&body_content);
-            match sudoku_puzzle {
-                Ok(sp) => {
-                    let grid = Sudoku::display(&sp.puzzle);
-                    match grid {
-                        Ok(s)   => future::ok(s),
-                        Err(e)  => future::err(e)
-                    }
-                }
-                _ => future::err(PuzzleError::InvalidGrid)
-            }
-        }
-        _ => future::err(PuzzleError::InvalidGrid)
+fn solve_sudoku(body: &BytesMut) -> Result<String, Box<dyn std::error::Error>> {
+    let body_content = std::str::from_utf8(body)?;
+
+    let sudoku_puzzle: Result<SudokuRequest, _> = serde_json::from_str(body_content);
+    match sudoku_puzzle {
+        Ok(sp) => Ok(Sudoku::new().solve(&sp.puzzle)?),
+        _ => Err(PuzzleError::InvalidGrid.into()),
     }
 }
 
-pub fn solve(body: Payload) -> impl Future<Item = HttpResponse, Error = Error> {
-    body.concat2()
-        .then(solve_sudoku)
-        .then(|solve_result| { 
-                    let sudoku_response = 
-                        match solve_result {
-                            Ok(solution)    => SolveResponse {status: "success".into(), data: solution, message: "".into()},
-                            Err(e)          => SolveResponse {status: "fail".into(), data: "".into(), message: format!("{}", e)}
-                        };
+fn display_sudoku(body: &BytesMut) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+    let body_content = std::str::from_utf8(body)?;
 
-                    let json_response = serde_json::to_string(&sudoku_response).unwrap();
-                    Ok(HttpResponse::Ok()
-                        .content_type("application/json")
-                        .body(json_response)
-                    )
-                }
-        )
+    let sudoku_puzzle: Result<SudokuRequest, _> = serde_json::from_str(body_content);
+    match sudoku_puzzle {
+        Ok(sp) => Ok(Sudoku::display(&sp.puzzle)?),
+        _ => Err(PuzzleError::InvalidGrid.into()),
+    }
 }
 
-pub fn display(body: Payload) -> impl Future<Item = HttpResponse, Error = Error> {
-    body.concat2()
-        .then(display_sudoku)
-        .then(|grid_result| { 
-                    let sudoku_response = 
-                        match grid_result {
-                            Ok(grid)    => DisplayResponse {status: "success".into(), data: grid, message: "".into()},
-                            Err(e)      => DisplayResponse {status: "fail".into(), data: Vec::new(), message: format!("{}", e)}
-                        };
+pub async fn solve(mut body: Payload) -> Result<HttpResponse, Error> {
+    let mut bytes = BytesMut::new();
+    while let Some(item) = body.next().await {
+        bytes.extend_from_slice(&item?);
+    }
 
-                    let json_response = serde_json::to_string(&sudoku_response).unwrap();
-                    Ok(HttpResponse::Ok()
-                        .content_type("application/json")
-                        .body(json_response)
-                    )
-                }
-        )
-} 
+    let solve_result = solve_sudoku(&bytes);
+    let sudoku_response = match solve_result {
+        Ok(solution) => SolveResponse {
+            status: "success".into(),
+            data: solution,
+            message: "".into(),
+        },
+        Err(e) => SolveResponse {
+            status: "fail".into(),
+            data: "".into(),
+            message: format!("{}", e),
+        },
+    };
 
+    let json_response = serde_json::to_string(&sudoku_response).unwrap();
+    Ok(HttpResponse::Ok()
+        .content_type("application/json")
+        .body(json_response))
+}
+
+pub async fn display(mut body: Payload) -> Result<HttpResponse, Error> {
+    let mut bytes = BytesMut::new();
+    while let Some(item) = body.next().await {
+        bytes.extend_from_slice(&item?);
+    }
+
+    let grid_result = display_sudoku(&bytes);
+    let sudoku_response = match grid_result {
+        Ok(grid) => DisplayResponse {
+            status: "success".into(),
+            data: grid,
+            message: "".into(),
+        },
+        Err(e) => DisplayResponse {
+            status: "fail".into(),
+            data: Vec::new(),
+            message: format!("{}", e),
+        },
+    };
+
+    let json_response = serde_json::to_string(&sudoku_response).unwrap();
+    Ok(HttpResponse::Ok()
+        .content_type("application/json")
+        .body(json_response))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,133 +1,152 @@
-use actix_web::{HttpServer, App, web::{scope, resource, post}};
+use actix_web::{
+    web::{post, resource, scope},
+    App, HttpServer,
+};
 
 mod handlers;
 use handlers::{display, solve};
 
-
-fn main() -> std::io::Result<()> {
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
     let addr = "localhost:7878";
     println!("Listening for requests at http://{}", addr);
 
-    HttpServer::new(|| App::new()
-                    .service(
-                        scope("/api")
-                        .service(resource("/solve").route(post().to_async(solve)))
-                        .service(resource("/display").route(post().to_async(display)))
-                    )
-                )
-                .bind(addr)
-                .unwrap()
-                .run()
+    HttpServer::new(|| {
+        App::new().service(
+            scope("/api")
+                .service(resource("/solve").route(post().to(solve)))
+                .service(resource("/display").route(post().to(display))),
+        )
+    })
+    .bind(addr)
+    .unwrap()
+    .run()
+    .await
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     use actix_web::{http, test};
     use bytes::Bytes;
-    use super::*;
 
     #[test]
+    #[allow(unused_must_use)]
     fn solve_ok() {
-        let mut app = test::init_service(
-            App::new().service(
-                        scope("/api")
-                        .service(resource("/solve").route(post().to_async(solve)))
-                        .service(resource("/display").route(post().to_async(display)))
-                        )
-                    );
+        async {
+            let mut app = test::init_service(
+                App::new().service(
+                    scope("/api")
+                        .service(resource("/solve").route(post().to(solve)))
+                        .service(resource("/display").route(post().to(display))),
+                ),
+            )
+            .await;
 
-        let req = test::TestRequest::with_header("content-type", "application/json")
-                .method(http::Method::POST)
-                .uri("/api/solve")
-                .set_payload(r#"{"puzzle": "700000600060001070804020005000470000089000340000039000600050709010300020003000004"}"#)
-                .to_request();
-       
-        let resp = test::call_service(&mut app, req);
-        assert!(resp.status().is_success());
+            let req = test::TestRequest::with_header("content-type", "application/json")
+                    .method(http::Method::POST)
+                    .uri("/api/solve")
+                    .set_payload(r#"{"puzzle": "700000600060001070804020005000470000089000340000039000600050709010300020003000004"}"#)
+                    .to_request();
 
-        let body = test::read_body(resp);
-        assert_eq!(body, Bytes::from_static(
-            r#"{"status":"success","data":"791543682562981473834726915356478291289615347147239568628154739415397826973862154","message":""}"#
-                .as_bytes())
-        ); 
+            let resp = test::call_service(&mut app, req).await;
+            assert!(resp.status().is_success());
+
+            let body = test::read_body(resp).await;
+            assert_eq!(body, Bytes::from_static(
+                r#"{"status":"success","data":"791543682562981473834726915356478291289615347147239568628154739415397826973862154","message":""}"#
+                    .as_bytes())
+            );
+        };
     }
 
- 
     #[test]
+    #[allow(unused_must_use)]
     fn display_ok() {
-        let mut app = test::init_service(
-            App::new().service(
-                        scope("/api")
-                        .service(resource("/solve").route(post().to_async(solve)))
-                        .service(resource("/display").route(post().to_async(display)))
-                        )
-                    );
+        async {
+            let mut app = test::init_service(
+                App::new().service(
+                    scope("/api")
+                        .service(resource("/solve").route(post().to(solve)))
+                        .service(resource("/display").route(post().to(display))),
+                ),
+            )
+            .await;
 
-        let req = test::TestRequest::with_header("content-type", "application/json")
-                .method(http::Method::POST)
-                .uri("/api/display")
-                .set_payload(r#"{"puzzle": "309800000000500000250009600480000097700000005930000061008300056000006000000007403"}"#)
-                .to_request();
+            let req = test::TestRequest::with_header("content-type", "application/json")
+                    .method(http::Method::POST)
+                    .uri("/api/display")
+                    .set_payload(r#"{"puzzle": "309800000000500000250009600480000097700000005930000061008300056000006000000007403"}"#)
+                    .to_request();
 
-        let resp = test::call_service(&mut app, req);
-        assert!(resp.status().is_success());
+            let resp = test::call_service(&mut app, req).await;
+            assert!(resp.status().is_success());
 
-        let body = test::read_body(resp);
-        assert_eq!(body, Bytes::from_static(
-            r#"{"status":"success","data":["3 0 9 |8 0 0 |0 0 0 ","0 0 0 |5 0 0 |0 0 0 ","2 5 0 |0 0 9 |6 0 0 ","------+------+------","4 8 0 |0 0 0 |0 9 7 ","7 0 0 |0 0 0 |0 0 5 ","9 3 0 |0 0 0 |0 6 1 ","------+------+------","0 0 8 |3 0 0 |0 5 6 ","0 0 0 |0 0 6 |0 0 0 ","0 0 0 |0 0 7 |4 0 3 "],"message":""}"#
-            .as_bytes())
-        );
+            let body = test::read_body(resp).await;
+            assert_eq!(body, Bytes::from_static(
+                r#"{"status":"success","data":["3 0 9 |8 0 0 |0 0 0 ","0 0 0 |5 0 0 |0 0 0 ","2 5 0 |0 0 9 |6 0 0 ","------+------+------","4 8 0 |0 0 0 |0 9 7 ","7 0 0 |0 0 0 |0 0 5 ","9 3 0 |0 0 0 |0 6 1 ","------+------+------","0 0 8 |3 0 0 |0 5 6 ","0 0 0 |0 0 6 |0 0 0 ","0 0 0 |0 0 7 |4 0 3 "],"message":""}"#
+                .as_bytes())
+            );
+        };
     }
 
     #[test]
+    #[allow(unused_must_use)]
     fn solve_err_puzzle() {
-        let mut app = test::init_service(
-            App::new().service(
-                        scope("/api")
-                        .service(resource("/solve").route(post().to_async(solve)))
-                        .service(resource("/display").route(post().to_async(display)))
-                        )
-                    );
+        async {
+            let mut app = test::init_service(
+                App::new().service(
+                    scope("/api")
+                        .service(resource("/solve").route(post().to(solve)))
+                        .service(resource("/display").route(post().to(display))),
+                ),
+            )
+            .await;
 
-        let req = test::TestRequest::with_header("content-type", "application/json")
+            let req = test::TestRequest::with_header("content-type", "application/json")
             .method(http::Method::POST)
             .uri("/api/solve")
             .set_payload(r#"{"puzzle": "X00000600060001070804020005000470000089000340000039000600050709010300020003000004"}"#)
             .to_request();
 
-        let resp = test::call_service(&mut app, req);
-        assert!(resp.status().is_success());
-        
-        let body = test::read_body(resp);
-        assert_eq!(body, Bytes::from_static(
+            let resp = test::call_service(&mut app, req).await;
+            assert!(resp.status().is_success());
+
+            let body = test::read_body(resp).await;
+            assert_eq!(body, Bytes::from_static(
             r#"{"status":"fail","data":"","message":"Invalid Grid.  Provide a string of 81 digits with 0 or . for empties."}"#
             .as_bytes())
-        );
+            );
+        };
     }
 
     #[test]
+    #[allow(unused_must_use)]
     fn solve_err_json() {
-        let mut app = test::init_service(
-            App::new().service(
-                        scope("/api")
-                        .service(resource("/solve").route(post().to_async(solve)))
-                        .service(resource("/display").route(post().to_async(display)))
-                        )
-                    );
+        async {
+            let mut app = test::init_service(
+                App::new().service(
+                    scope("/api")
+                        .service(resource("/solve").route(post().to(solve)))
+                        .service(resource("/display").route(post().to(display))),
+                ),
+            )
+            .await;
 
-        let req = test::TestRequest::with_header("content-type", "application/json")
+            let req = test::TestRequest::with_header("content-type", "application/json")
                 .method(http::Method::POST)
                 .uri("/api/solve")
                 .set_payload(r#"{"xuzzle": "700000600060001070804020005000470000089000340000039000600050709010300020003000004"}"#)
                 .to_request();
 
-        let resp = test::call_service(&mut app, req);
-        assert!(resp.status().is_success());
+            let resp = test::call_service(&mut app, req).await;
+            assert!(resp.status().is_success());
 
-        let body = test::read_body(resp);
-        assert_eq!(body, Bytes::from_static(
+            let body = test::read_body(resp).await;
+            assert_eq!(body, Bytes::from_static(
             r#"{"status":"fail","data":"","message":"Invalid Grid.  Provide a string of 81 digits with 0 or . for empties."}"#
             .as_bytes())
         );
+        };
     }
 }
